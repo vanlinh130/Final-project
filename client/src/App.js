88b51c7fc4c6a72f70d269385184ea8db4c543df
vanlinh1330 <levanlinh1330@gamil.com>
2023-01-17 00:33:16 +0700

    client : Xử Lý Ui CompareProducts, Wishlist, Login

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import OurStore from './pages/OurStore';
 import Blogs from './pages/Blogs';
+import CompareProducts from './pages/CompareProducts';
+import Wishlist from './pages/Wishlist';
+import Login from './pages/Login';
 
 function App() {
     return (
@@ -15,10 +18,13 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route index element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="/store" element={<OurStore />} />
-                        <Route path="/blogs" element={<Blogs />} />
+                        <Route path="about" element={<About />} />
+                        <Route path="contact" element={<Contact />} />
+                        <Route path="store" element={<OurStore />} />
+                        <Route path="blogs" element={<Blogs />} />
+                        <Route path="compare-product" element={<CompareProducts />} />
+                        <Route path="wishlist" element={<Wishlist />} />
+                        <Route path="login" element={<Login />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
--- client/src/pages/OurStore.js
+++ client/src/pages/OurStore.js
@@ -3,6 +3,7 @@ import BreadCrumb from '../components/BreadCrumb';
 import Meta from '../components/Meta';
 import ReactStars from 'react-rating-stars-component';
 import ProductCard from './../components/ProductCard';
+import Color from '../components/Color';
 
 const OurStore = () => {
     const [grid, setGrid] = useState(3);
@@ -71,19 +72,7 @@ const OurStore = () => {
                                     </div>
                                     <h5 className="sub-title">Colors</h5>
                                     <div>
-                                        <ul className="colors ps-0">
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                        </ul>
+                                        <Color />
                                     </div>
                                     <div className="sub-title">Size</div>
                                     <div>
