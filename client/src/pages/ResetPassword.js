bf6084cbdeeb03dc6f79aba55519bac5fac5d205
vanlinh1330 <levanlinh1330@gamil.com>
2023-01-29 18:06:11 +0700

    update Input all

--- client/src/pages/ResetPassword.js
+++ client/src/pages/ResetPassword.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BreadCrumb from '../components/BreadCrumb';
 import Meta from '../components/Meta';
 import Container from './../components/Container';
+import CustomInput from '../components/CustomInput';
 
 const ResetPassword = () => {
     return (
@@ -14,22 +15,8 @@ const ResetPassword = () => {
                         <div className="auth-card">
                             <h3 className="text-center mb-3">Reset Password</h3>
                             <form action="" className="d-flex flex-column gap-15">
-                                <div>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        placeholder="Password"
-                                        className="form-control"
-                                    />
-                                </div>
-                                <div>
-                                    <input
-                                        type="password"
-                                        name="confpassword"
-                                        placeholder="Confirm Password"
-                                        className="form-control"
-                                    />
-                                </div>
+                                <CustomInput type="password" name="password" placeholder="Password" />
+                                <CustomInput type="password" name="confpassword" placeholder="Confirm Password" />
                                 <div>
                                     <div className="mt-3 d-flex flex-column gap-15 align-items-center">
                                         <button className="button border-0">Ok</button>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BreadCrumb from '../components/BreadCrumb';
 import Meta from '../components/Meta';
 import Container from './../components/Container';
+import CustomInput from '../components/CustomInput';
 
 const Signup = () => {
     return (
@@ -14,28 +15,10 @@ const Signup = () => {
                         <div className="auth-card">
                             <h3 className="text-center mb-3">Sign Up</h3>
                             <form action="" className="d-flex flex-column gap-15">
-                                <div>
-                                    <input type="text" name="name" placeholder="Name" className="form-control" />
-                                </div>
-                                <div>
-                                    <input type="email" name="email" placeholder="Email" className="form-control" />
-                                </div>
-                                <div>
-                                    <input
-                                        type="tel"
-                                        name="mobile"
-                                        placeholder="Mobile Number"
-                                        className="form-control"
-                                    />
-                                </div>
-                                <div className="mt-1">
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        placeholder="Password"
-                                        className="form-control"
-                                    />
-                                </div>
+                                <CustomInput type="text" name="name" placeholder="Name" />
+                                <CustomInput type="email" name="email" placeholder="Email" />
+                                <CustomInput type="tel" name="mobile" placeholder="Mobile Number" />
+                                <CustomInput type="password" name="password" placeholder="Password" />
                                 <div>
                                     <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
                                         <button className="button border-0">Sign Up</button>
