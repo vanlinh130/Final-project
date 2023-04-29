de839396b0e5ddfa9177dbd13d3b6127ec85e7fe
Văn Linh Lê <linhvanle130@gmail.com>
2023-04-30 01:49:52 +0700

    Server block, unblock

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -93,4 +93,54 @@ const deleteaUser = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser };
+//
+const blockUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const block = await User.findByIdAndUpdate(
+            id,
+            {
+                isBlocked: true,
+            },
+            {
+                new: true,
+            },
+        );
+        res.json({
+            message: 'User Blocked',
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const unblockUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const unblock = await User.findByIdAndUpdate(
+            id,
+            {
+                isBlocked: false,
+            },
+            {
+                new: true,
+            },
+        );
+        res.json({
+            message: 'User UnBlocked',
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = {
+    createUser,
+    loginUserCtrl,
+    getallUser,
+    getaUser,
+    deleteaUser,
+    updatedUser,
+    blockUser,
+    unblockUser,
+};
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,5 +1,14 @@
 const express = require('express');
-const { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser } = require('../controller/userCtrl');
+const {
+    createUser,
+    loginUserCtrl,
+    getallUser,
+    getaUser,
+    deleteaUser,
+    updatedUser,
+    blockUser,
+    unblockUser,
+} = require('../controller/userCtrl');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
@@ -9,4 +18,6 @@ router.get('/all-users', getallUser);
 router.get('/:id', authMiddleware, isAdmin, getaUser);
 router.delete('/:id', deleteaUser);
 router.put('/edit-user', authMiddleware, updatedUser);
+router.put('/block-user/:id', authMiddleware, isAdmin, blockUser);
+router.put('/unblock-user/:id', authMiddleware, isAdmin, unblockUser);
 module.exports = router;
