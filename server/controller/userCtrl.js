8120d301e6add837a02eb2128fc0fad38a142b9a
Văn Linh Lê <linhvanle130@gmail.com>
2023-04-29 22:26:37 +0700

    server: all users, a user,delete,update ,done auth

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -1,6 +1,8 @@
+const { generateToken } = require('../config/jwtToken');
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
 
+// create a user
 const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({ email: email });
@@ -13,15 +15,81 @@ const createUser = asyncHandler(async (req, res) => {
     }
 });
 
+// login a user
 const loginUserCtrl = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     // check if user exists or not
     const findUser = await User.findOne({ email });
     if (findUser && (await findUser.isPasswordMatched(password))) {
-        res.json(findUser);
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token: generateToken(findUser?._id),
+        });
     } else {
         throw new Error('Invalid Credentials');
     }
 });
 
-module.exports = { createUser, loginUserCtrl };
+// Update a user
+const updatedUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            {
+                firstname: req?.body?.firstname,
+                lastname: req?.body?.lastname,
+                email: req?.body?.email,
+                mobile: req?.body?.mobile,
+            },
+            {
+                new: true,
+            },
+        );
+        res.json(updatedUser);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+// Get all users
+const getallUser = asyncHandler(async (req, res) => {
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+// Get a single user
+const getaUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const getaUser = await User.findById(id);
+        res.json({
+            getaUser,
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+// Delete a single user
+const deleteaUser = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleteaUser = await User.findByIdAndDelete(id);
+        res.json({
+            deleteaUser,
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser };
