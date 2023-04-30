7940f62c241e55e49665bf5eaaace1df8fb01d0b
Văn Linh Lê <linhvanle130@gmail.com>
2023-04-30 18:14:25 +0700

    Server CreateProduct

---
+++ server/controller/productCtrl.js
@@ -0,0 +1,13 @@
+const Product = require('../models/productModel');
+const asyncHandler = require('express-async-handler');
+
+const createProduct = asyncHandler(async (req, res) => {
+    try {
+        const newProduct = await Product.create(req.body);
+        res.json(newProduct);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = { createProduct };
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute');
+const productRouter = require('./routes/productRoute');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
 dbConnect();
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api/user', authRouter);
+app.use('/api/product', productRouter);
 
 app.use(notFound);
 app.use(errorHandler);
