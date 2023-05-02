0ad9f37c48df6516cd9e79f710d47b77f87a790c
Văn Linh Lê <linhvanle130@gmail.com>
2023-05-03 01:02:40 +0700

    Server resetPassword

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -3,6 +3,7 @@ const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
 const validateMongoDbId = require('../utils/validateMongodbid');
 const { generateRefreshToken } = require('../config/refreshtoken');
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const sendEmail = require('./emailCtrl');
 
@@ -233,6 +234,22 @@ const forgotPasswordToken = asyncHandler(async (req, res) => {
     }
 });
 
+const resetPassword = asyncHandler(async (req, res) => {
+    const { password } = req.body;
+    const { token } = req.params;
+    const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+    const user = await User.findOne({
+        passwordResetToken: hashedToken,
+        passwordResetExpires: { $gt: Date.now() },
+    });
+    if (!user) throw new Error('Token Expired, Please try again later');
+    user.password = password;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+    res.json(user);
+});
+
 module.exports = {
     createUser,
     loginUserCtrl,
@@ -246,4 +263,5 @@ module.exports = {
     layout,
     updatePassword,
     forgotPasswordToken,
+    resetPassword,
 };
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -12,14 +12,16 @@ const {
     layout,
     updatePassword,
     forgotPasswordToken,
+    resetPassword,
 } = require('../controller/userCtrl');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 router.post('/register', createUser);
 router.post('/forgot-password-token', forgotPasswordToken);
-router.put('/password', authMiddleware, updatePassword);
+router.put('/reset-password/:token', resetPassword);
 
+router.put('/password', authMiddleware, updatePassword);
 router.post('/login', loginUserCtrl);
 router.get('/all-users', getallUser);
 router.get('/refresh', handleRefreshToken);
