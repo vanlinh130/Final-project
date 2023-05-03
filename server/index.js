acdeeb585ddaacfa011c9c56d77f880cb0e528c3
Văn Linh Lê <linhvanle130@gmail.com>
2023-05-03 15:36:38 +0700

    Server Prod Category

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
 const blogRouter = require('./routes/blogRoute');
+const categoryRouter = require('./routes/prodcategoryRoute');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use('/api/user', authRouter);
 app.use('/api/product', productRouter);
 app.use('/api/blog', blogRouter);
+app.use('/api/category', categoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
