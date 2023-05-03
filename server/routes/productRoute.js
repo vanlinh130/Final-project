67e76d266e5a4024a3bb0a39b24fee3a000c9cbd
Văn Linh Lê <linhvanle130@gmail.com>
2023-05-04 00:21:06 +0700

    Server Product rating

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -6,6 +6,7 @@ const {
     updateProduct,
     deleteProduct,
     addToWishlist,
+    rating,
 } = require('../controller/productCtrl');
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
 const router = express.Router();
@@ -13,6 +14,7 @@ const router = express.Router();
 router.post('/', authMiddleware, isAdmin, createProduct);
 router.get('/:id', getaProduct);
 router.put('/wishlist', authMiddleware, addToWishlist);
+router.put('/rating', authMiddleware, rating);
 
 router.put('/:id', authMiddleware, isAdmin, updateProduct);
 router.delete('/:id', authMiddleware, isAdmin, deleteProduct);
