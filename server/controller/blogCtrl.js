a2d68ff64a711a8c81152a891925ed45e501d201
Văn Linh Lê <linhvanle130@gmail.com>
2023-05-03 16:20:33 +0700

    Server Blog Category

--- server/controller/blogCtrl.js
+++ server/controller/blogCtrl.js
@@ -1,4 +1,4 @@
-const Blog = require('../models/blogMode');
+const Blog = require('../models/blogModel');
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
 const validateMongoDbId = require('../utils/validateMongodbid');
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const authRouter = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
 const blogRouter = require('./routes/blogRoute');
 const categoryRouter = require('./routes/prodcategoryRoute');
+const blogcategoryRouter = require('./routes/blogCatRoute');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
@@ -22,6 +23,7 @@ app.use('/api/user', authRouter);
 app.use('/api/product', productRouter);
 app.use('/api/blog', blogRouter);
 app.use('/api/category', categoryRouter);
+app.use('/api/blogcategory', blogcategoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
