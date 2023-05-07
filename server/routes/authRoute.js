8dd24fcb7ea66ef6a387969a4a3322418d1d37b9
Văn Linh Lê <linhvanle130@gmail.com>
2023-05-08 00:25:35 +0700

    Server UserCart

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -16,6 +16,7 @@ const {
     loginAdmin,
     getWishlist,
     saveAddress,
+    userCart,
 } = require('../controller/userCtrl');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 
@@ -27,6 +28,7 @@ router.put('/reset-password/:token', resetPassword);
 router.put('/password', authMiddleware, updatePassword);
 router.post('/login', loginUserCtrl);
 router.post('/admin-login', loginAdmin);
+router.post('/cart', authMiddleware, userCart);
 router.get('/all-users', getallUser);
 router.get('/refresh', handleRefreshToken);
 router.get('/layout', layout);
