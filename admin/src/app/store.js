6fc451708fc834753bc928c5e109fbfe8557eddd
Văn Linh Lê <linhvanle130@gmail.com>
2023-05-09 23:12:56 +0700

    Admin Api Brand list

--- admin/src/app/store.js
+++ admin/src/app/store.js
@@ -2,7 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import customerReducer from '../features/customers/customersSlice';
 import productReducer from '../features/product/productSlice';
+import brandReducer from '../features/brand/brandSlide';
 
 export const store = configureStore({
-    reducer: { auth: authReducer, customer: customerReducer, product: productReducer },
+    reducer: { auth: authReducer, customer: customerReducer, product: productReducer, brand: brandReducer },
 });
--- admin/src/pages/BrandList.js
+++ admin/src/pages/BrandList.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Table } from 'antd';
+import { BiEdit } from 'react-icons/bi';
+import { AiFillDelete } from 'react-icons/ai';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBrands } from '../features/brand/brandSlide';
+import { Link } from 'react-router-dom';
 
 const columns = [
     {
@@ -7,30 +12,41 @@ const columns = [
         dataIndex: 'key',
     },
     {
-        title: 'Name',
-        dataIndex: 'name',
+        title: 'Title',
+        dataIndex: 'title',
     },
     {
-        title: 'Product',
-        dataIndex: 'product',
-    },
-    {
-        title: 'Status',
-        dataIndex: 'status',
+        title: 'Action',
+        dataIndex: 'action',
     },
 ];
 
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-    data1.push({
-        key: i,
-        name: `Edward King ${i}`,
-        product: 32,
-        status: `London, Park Lane no. ${i}`,
-    });
-}
-
 const BrandList = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getBrands());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const brandState = useSelector((state) => state.brand.brands);
+
+    const data1 = [];
+    for (let i = 0; i < brandState.length; i++) {
+        data1.push({
+            key: i + 1,
+            title: brandState[i].title,
+            action: (
+                <>
+                    <Link to="/" className="fs-3 text-danger ">
+                        <BiEdit />
+                    </Link>
+                    <Link to="/" className="fs-3 text-danger ms-3">
+                        <AiFillDelete />
+                    </Link>
+                </>
+            ),
+        });
+    }
     return (
         <div>
             <h3 className="mb-4 title">Brands</h3>
