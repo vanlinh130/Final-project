38a374aab3d2169149925fb9ef5b0a10c70bf89d
Văn Linh Lê <linhvanle130@gmail.com>
2023-05-09 22:46:23 +0700

    Admin Api Product List

--- admin/src/app/store.js
+++ admin/src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import customerReducer from '../features/customers/customersSlice';
+import productReducer from '../features/product/productSlice';
 
 export const store = configureStore({
-    reducer: { auth: authReducer, customer: customerReducer },
+    reducer: { auth: authReducer, customer: customerReducer, product: productReducer },
 });
