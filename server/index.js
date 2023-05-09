65c694f45a36d856146042dce7a5f3dc522dc7f2
Văn Linh Lê <linhvanle130@gmail.com>
2023-05-09 18:59:37 +0700

    Server Ket noi voi Admin

--- server/index.js
+++ server/index.js
@@ -14,9 +14,12 @@ const couponRouter = require('./routes/couponRoute');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const cors = require('cors');
+
 dbConnect();
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
