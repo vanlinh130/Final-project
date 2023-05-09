4df70cabe2bd2ae656c8bc95448435c673711216
Văn Linh Lê <linhvanle130@gmail.com>
2023-05-10 00:42:18 +0700

    Admin Blog bcategory

--- admin/src/pages/BlogCatList.js
+++ admin/src/pages/BlogCatList.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Table } from 'antd';
+import { BiEdit } from 'react-icons/bi';
+import { AiFillDelete } from 'react-icons/ai';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getCategories } from '../features/bcategory/bcategorySlide';
 
 const columns = [
     {
@@ -7,30 +12,43 @@ const columns = [
         dataIndex: 'key',
     },
     {
-        title: 'Name',
-        dataIndex: 'name',
+        title: 'Title',
+        dataIndex: 'title',
+        sorter: (a, b) => a.title.length - b.title.length,
     },
     {
-        title: 'Product',
-        dataIndex: 'product',
-    },
-    {
-        title: 'Status',
-        dataIndex: 'status',
+        title: 'Action',
+        dataIndex: 'action',
     },
 ];
 
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-    data1.push({
-        key: i,
-        name: `Edward King ${i}`,
-        product: 32,
-        status: `London, Park Lane no. ${i}`,
-    });
-}
-
 const BlogCatList = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getCategories());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const bCatState = useSelector((state) => state.bCategory.bCategories);
+
+    const data1 = [];
+    for (let i = 0; i < bCatState.length; i++) {
+        data1.push({
+            key: i + 1,
+            title: bCatState[i].title,
+            action: (
+                <>
+                    <Link to="/" className="fs-3 text-danger ">
+                        <BiEdit />
+                    </Link>
+                    <Link to="/" className="fs-3 text-danger ms-3">
+                        <AiFillDelete />
+                    </Link>
+                </>
+            ),
+        });
+    }
+
     return (
         <div>
             <h3 className="mb-4 title">Blog Category List</h3>
